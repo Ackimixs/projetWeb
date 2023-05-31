67387841170f78ba8c99fdaa610a4349a68b23a6
Julia Azeau Bigorgne <julia.azeau-bigorgne@isen-ouest.yncrea.fr>
2023-05-31 16:58:38 +0200

    user

--- php/User.php
+++ php/User.php
@@ -2,5 +2,108 @@
 
 class User
 {
+    static function authentification($email, $mdp)
+    {
+        $db = dbConnect();
+        $request = 'SELECT "user".id_user FROM "user" WHERE "user".mail = :email AND "user".motdepasse = crypt(:mdp, motdepasse)';
+        $stmt = $db->prepare($request);
+        $stmt->bindParam(':email', $email);
+        $stmt->bindParam(':mdp', $mdp);
+        $stmt->execute();
+        $resultat = $stmt->fetch();
+        return $resultat;
+    }
+    //
+    // Renvoie toutes les user
+    //
+    static function getUsers()
+    {
+        $db = dbConnect();
+        $request = 'SELECT * FROM "user"';
+        $query = $db->prepare($request);
+        $query->execute();
+        $resultat = $query->fetchAll(PDO::FETCH_ASSOC);
+        return $resultat;
+    }
 
+    //
+    // Renvoie un user en particulier
+    //
+    static function getUnUser($id)
+    {
+        $db = dbConnect();
+        $request = 'SELECT * FROM "user"
+                    WHERE "user".id_user = :id';
+        $query = $db->prepare($request);
+        $query->bindParam(':id', $id);
+        $query->execute();
+        $resultat = $query->fetchAll(PDO::FETCH_ASSOC);
+        return $resultat;
+    }
+    //
+    // Renvoie les playlist d'un user
+    //
+    static function playlistUser($id)
+    {
+        $db = dbConnect();
+        $request = 'SELECT * FROM "user"
+                    INNER JOIN user_playlist up on up.id_user = "user".id_user
+                    INNER JOIN playlist p on p.id_playlist = up.id_playlist
+                    WHERE "user".id_user = :id';
+        $query = $db->prepare($request);
+        $query->bindParam(':id', $id);
+        $query->execute();
+        $resultat = $query->fetchAll(PDO::FETCH_ASSOC);
+        return $resultat;
+    }
+
+    //
+    // Modifie les parametres de l'utilisateur
+    //
+    static function modificationUser($id, $parametre, $valeur)
+    {
+        $db = dbConnect();
+        $request = 'UPDATE "user"
+                    SET :parametre = :valeur
+                    WHERE id_user = :iduser';
+        $query = $db->prepare($request);
+        $query->bindParam(':iduser', $id);
+        $query->bindParam(':parametre', $parametre);
+        $query->bindParam(':valeur', $valeur);
+        $query->execute();
+        $query->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    //
+    // Renvoie l'hitorique d'un utilisateur
+    //
+    static function historiqueUser($id)
+    {
+        $db = dbConnect();
+        $request = 'SELECT * FROM "user"
+                    INNER JOIN historique h on h.id_user = "user".id_user
+                    INNER JOIN musique m on m.id_musique = h.id_musique
+                    WHERE "user".id_user = 2
+                    LIMIT 10';
+        $query = $db->prepare($request);
+        $query->bindParam(':id', $id);
+        $query->execute();
+        $resultat = $query->fetchAll(PDO::FETCH_ASSOC);
+        return $resultat;
+    }
+
+    //
+    // Ajoute une musique a l'historique de l'utilisateur
+    //
+    static function ajoutHistorique($iduser, $idmusique)
+    {
+        $db = dbConnect();
+        $request = "INSERT INTO historique (id_user, id_musique)
+                    VALUES(:iduser, :idmus)";
+        $query = $db->prepare($request);
+        $query->bindParam(':iduser', $iduser);
+        $query->bindParam(':idmus', $idmusique);
+        $query->execute();
+        $query->fetchAll(PDO::FETCH_ASSOC);
+    }
 }
