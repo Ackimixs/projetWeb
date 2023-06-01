0a93192b22c2ebfcc13f42267438658811afe767
Julia Azeau Bigorgne <julia.azeau-bigorgne@isen-ouest.yncrea.fr>
2023-06-01 19:23:02 +0200

    class propre

--- php/User.php
+++ php/User.php
@@ -4,26 +4,37 @@ class User
 {
     static function authentification($email, $mdp)
     {
-        $db = $db = Db::connectionDB();
-        $request = 'SELECT "user".id_user FROM "user" WHERE "user".mail = :email AND "user".motdepasse = crypt(:mdp, motdepasse)';
-        $stmt = $db->prepare($request);
-        $stmt->bindParam(':email', $email);
-        $stmt->bindParam(':mdp', $mdp);
-        $stmt->execute();
-        $resultat = $stmt->fetch();
-        return $resultat;
+        try {
+            $db = $db = Db::connectionDB();
+            $request = 'SELECT "user".id_user FROM "user" WHERE "user".mail = :email AND "user".motdepasse = crypt(:mdp, motdepasse)';
+            $stmt = $db->prepare($request);
+            $stmt->bindParam(':email', $email);
+            $stmt->bindParam(':mdp', $mdp);
+            $stmt->execute();
+            return $stmt->fetch();
+        }
+        catch (PDOException $exception){
+            error_log($exception->getMessage());
+            return false;
+        }
     }
     //
     // Renvoie toutes les user
     //
     static function getUsers()
     {
-        $db = Db::connectionDB();dbConnect();
-        $request = 'SELECT * FROM "user"';
-        $query = $db->prepare($request);
-        $query->execute();
-        $resultat = $query->fetchAll(PDO::FETCH_ASSOC);
-        return $resultat;
+        try {
+            $db = Db::connectionDB();
+            dbConnect();
+            $request = 'SELECT * FROM "user"';
+            $stmt = $db->prepare($request);
+            $stmt->execute();
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
+        catch (PDOException $exception){
+            error_log($exception->getMessage());
+            return false;
+        }
     }
 
     //
@@ -31,65 +42,86 @@ class User
     //
     static function getUnUser($id)
     {
-        $db = $db = Db::connectionDB();
-        $request = 'SELECT * FROM "user"
+        try {
+            $db = $db = Db::connectionDB();
+            $request = 'SELECT * FROM "user"
                     WHERE "user".id_user = :id';
-        $query = $db->prepare($request);
-        $query->bindParam(':id', $id);
-        $query->execute();
-        $resultat = $query->fetchAll(PDO::FETCH_ASSOC);
-        return $resultat;
+            $stmt = $db->prepare($request);
+            $stmt->bindParam(':id', $id);
+            $stmt->execute();
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
+        catch (PDOException $exception){
+            error_log($exception->getMessage());
+            return false;
+        }
     }
     //
     // Renvoie les playlist d'un user
     //
     static function playlistUser($id)
     {
-        $db = Db::connectionDB();
-        $request = 'SELECT * FROM "user"
+        try {
+            $db = Db::connectionDB();
+            $request = 'SELECT * FROM "user"
                     INNER JOIN user_playlist up on up.id_user = "user".id_user
                     INNER JOIN playlist p on p.id_playlist = up.id_playlist
                     WHERE "user".id_user = :id';
-        $query = $db->prepare($request);
-        $query->bindParam(':id', $id);
-        $query->execute();
-        $resultat = $query->fetchAll(PDO::FETCH_ASSOC);
-        return $resultat;
+            $stmt = $db->prepare($request);
+            $stmt->bindParam(':id', $id);
+            $stmt->execute();
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
+        catch (PDOException $exception){
+            error_log($exception->getMessage());
+            return false;
+        }
     }
 
     //
     // Modifie les parametres de l'utilisateur
     //
-    static function modificationUser($id, $parametre, $valeur)
+    /*static function modificationUser($id, $parametre, $valeur)
     {
-        $db = Db::connectionDB();
-        $request = 'UPDATE "user"
+        try {
+            $db = Db::connectionDB();
+            $request = 'UPDATE "user"
                     SET :parametre = :valeur
                     WHERE id_user = :iduser';
-        $query = $db->prepare($request);
-        $query->bindParam(':iduser', $id);
-        $query->bindParam(':parametre', $parametre);
-        $query->bindParam(':valeur', $valeur);
-        $query->execute();
-        $query->fetchAll(PDO::FETCH_ASSOC);
-    }
+            $stmt = $db->prepare($request);
+            $stmt->bindParam(':iduser', $id);
+            $stmt->bindParam(':parametre', $parametre);
+            $stmt->bindParam(':valeur', $valeur);
+            $stmt->execute();
+            $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
+        catch (PDOException $exception){
+            error_log($exception->getMessage());
+            return false;
+        }
+    }*/
 
     //
     // Renvoie l'hitorique d'un utilisateur
     //
     static function historiqueUser($id)
     {
-        $db = Db::connectionDB();
-        $request = 'SELECT * FROM "user"
+        try {
+            $db = Db::connectionDB();
+            $request = 'SELECT * FROM "user"
                     INNER JOIN historique h on h.id_user = "user".id_user
                     INNER JOIN musique m on m.id_musique = h.id_musique
                     WHERE "user".id_user = 2
                     LIMIT 10';
-        $query = $db->prepare($request);
-        $query->bindParam(':id', $id);
-        $query->execute();
-        $resultat = $query->fetchAll(PDO::FETCH_ASSOC);
-        return $resultat;
+            $stmt = $db->prepare($request);
+            $stmt->bindParam(':id', $id);
+            $stmt->execute();
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
+        catch (PDOException $exception){
+            error_log($exception->getMessage());
+            return false;
+        }
     }
 
     //
@@ -97,13 +129,19 @@ class User
     //
     static function ajoutHistorique($iduser, $idmusique)
     {
-        $db = Db::connectionDB();
-        $request = "INSERT INTO historique (id_user, id_musique)
+        try {
+            $db = Db::connectionDB();
+            $request = "INSERT INTO historique (id_user, id_musique)
                     VALUES(:iduser, :idmus)";
-        $query = $db->prepare($request);
-        $query->bindParam(':iduser', $iduser);
-        $query->bindParam(':idmus', $idmusique);
-        $query->execute();
-        $query->fetchAll(PDO::FETCH_ASSOC);
+            $stmt = $db->prepare($request);
+            $stmt->bindParam(':iduser', $iduser);
+            $stmt->bindParam(':idmus', $idmusique);
+            $stmt->execute();
+            $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
+        catch (PDOException $exception){
+            error_log($exception->getMessage());
+            return false;
+        }
     }
 }
