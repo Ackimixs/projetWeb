4f2abb53e5a992d500bc0a5437955c992b43e70a
ackimixs <ackimixs@protonmail.com>
2023-06-03 11:50:06 +0200

    add full keyboard control for the audio player

--- js/musiquePlayer.js
+++ js/musiquePlayer.js
@@ -1,4 +1,5 @@
 let autoPlay = false;
+let muted = false;
 let audioTag = document.querySelector('#audioTag');
 let playButton = document.querySelector('.playButton');
 let pauseButton = document.querySelector('.pauseButton');
@@ -12,9 +13,13 @@ let previousButton = document.querySelector('.previousButton');
 const volumeSlider = document.querySelector('.volume-slider');
 
 const audioMuted = document.querySelector('#audioMuted');
-const audioNotMuted = document.querySelector('#audioNotMuted');
+const audioNotMuted = document.querySelectorAll('.audioNotMuted');
 
 const setMusique = (musique) => {
+
+    ajaxRequest("POST", "../php/request.php/historique", () => {}, `id=${musique.id_musique}`);
+
+    console.log("Playing", musique.id_musique)
 
     musiqueName.innerText = musique.titre_musique + " - " + musique.nom_artiste;
 
@@ -73,8 +78,38 @@ pauseButton.addEventListener('click', () => {
     autoPlay = false;
 })
 
+document.addEventListener("keydown", (event) => {
+    console.log(event.code);
+    if (event.code === "Space") {
+        if (audioTag.paused) {
+            audioTag.play();
+            playButton.hidden = true;
+            pauseButton.hidden = false;
+        } else {
+            audioTag.pause();
+            playButton.hidden = false;
+            pauseButton.hidden = true;
+        }
+    }
+    else if (event.code === "ArrowRight") {
+        audioTag.currentTime += 5;
+    }
+    else if (event.code === "ArrowLeft") {
+        audioTag.currentTime -= 5;
+    }
+    else if (event.code === "ArrowUp") {
+        handleVolume((audioTag.volume * 100 + 5) / 100);
+    }
+    else if (event.code === "ArrowDown") {
+        handleVolume((audioTag.volume * 100 - 5) / 100);
+    }
+    else if (event.code === "Semicolon") {
+        handleMuted();
+    }
+})
+
 previousButton.addEventListener('click', () => {
-    getMusique(getRandomInteger(1, 200))
+    ajaxRequest("GET", '../php/request.php/historique', setLastSong);
 })
 
 nextButton.addEventListener('click', () => {
@@ -82,22 +117,29 @@ nextButton.addEventListener('click', () => {
 })
 
 audioMuted.addEventListener('click', () => {
-    audioMuted.hidden = true;
-    audioNotMuted.hidden = false;
-    volumeSlider.disabled = false;
-    audioTag.volume = volumeSlider.value / 100;
+    handleVolume(volumeSlider.value / 100);
+    muted = false;
 });
 
-audioNotMuted.addEventListener('click', () => {
-    audioMuted.hidden = false;
-    audioNotMuted.hidden = true;
-    volumeSlider.disabled = true;
-    audioTag.volume = 0;
+audioNotMuted.forEach((element) => {
+    element.addEventListener('click', () => {
+        audioNotMuted.forEach(e => e.hidden = true);
+        audioMuted.hidden = false;
+        volumeSlider.disabled = true;
+        audioTag.volume = 0;
+        muted = true;
+    });
 })
 
 volumeSlider.addEventListener('change', () => {
-  audioTag.volume = volumeSlider.value / 100;
+    handleVolume(volumeSlider.value / 100);
 });
+
+function setLastSong(data) {
+    if (data.length > 1) {
+        getMusique(data[1].id_musique)
+    }
+}
 
 function parseSeconds(seconds) {
     const minutes = Math.floor(seconds / 60);
@@ -116,5 +158,63 @@ function getRandomInteger(min, max) {
 function getMusique(id_musique) {
     ajaxRequest("GET", "../php/request.php/musique/" + id_musique, setMusique);
 }
+
+
+function handleVolume(volume) {
+    if (volume < 0.0 || volume > 1) return;
+    if (muted)  {
+        volume = volumeSlider.value / 100;
+        volumeSlider.disabled = false;
+        muted = !muted;
+    }
+
+    audioTag.volume = volume;
+    if (volume <= 0) {
+        audioMuted.hidden = false;
+        audioNotMuted.forEach(e => e.hidden = true);
+    } else if (volume < 0.35) {
+        audioMuted.hidden = true;
+        audioNotMuted[0].hidden = false;
+        audioNotMuted[1].hidden = true;
+        audioNotMuted[2].hidden = true;
+    } else if (volume < 0.7) {
+        audioMuted.hidden = true;
+        audioNotMuted[0].hidden = true;
+        audioNotMuted[1].hidden = false;
+        audioNotMuted[2].hidden = true;
+    } else {
+        audioMuted.hidden = true;
+        audioNotMuted[0].hidden = true;
+        audioNotMuted[1].hidden = true;
+        audioNotMuted[2].hidden = false;
+    }
+    volumeSlider.value = volume * 100;
+}
+
+function handleMuted() {
+    if (audioTag.volume === 0) {
+        audioTag.volume = volumeSlider.value / 100;
+        audioMuted.hidden = true;
+        audioNotMuted.forEach(e => e.hidden = true);
+        if (audioTag.volume < 0.35) {
+            audioNotMuted[0].hidden = false;
+        } else if (audioTag.volume < 0.7) {
+            audioNotMuted[1].hidden = false;
+        } else {
+            audioNotMuted[2].hidden = false;
+        }
+        volumeSlider.disabled = false;
+        muted = false;
+    } else {
+        audioTag.volume = 0;
+        audioMuted.hidden = false;
+        audioNotMuted.forEach(e => e.hidden = true);
+        volumeSlider.disabled = true;
+        muted = true;
+    }
+}
+
+
+
 
 getMusique(36);
