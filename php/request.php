fc428b68b1d3ee9542315994d21b23bcfcb6ba0d
ackimixs <ackimixs@protonmail.com>
2023-06-03 11:49:52 +0200

    add player in the api route

--- php/request.php
+++ php/request.php
@@ -5,6 +5,7 @@ require_once('Playlist.php');
 require_once('Musique.php');
 require_once('Artiste.php');
 require_once('Album.php');
+require_once('User.php');
 
 session_start();
 
@@ -64,7 +65,11 @@ switch($requestRessource)
             case 'GET':
                 if ($id != null) {
                     if ($id == "session") {
-                        $data = $_SESSION['user'];
+                        if (isset($_SESSION["user"])) {
+                            $data = $_SESSION['user'];
+                        } else {
+                            $data = false;
+                        }
                     }
                 }
                 break;
@@ -73,9 +78,10 @@ switch($requestRessource)
                 header("HTTP/1.0 405 Method Not Allowed");
                 break;
         }
+        break;
     case 'recherchemusique':
         //print_r('get :'.$_GET['stringrecherche']);
-        $data = Musique::rechercheMusiques($_GET['stringrecherche']);
+        $data = array(); //Musique::rechercheMusiques($_GET['stringrecherche']);
         break;
     case 'rechercheplaylist':
         $data = Playlist::recherchePlaylists($_GET['stringrecherche']);
@@ -86,12 +92,39 @@ switch($requestRessource)
     case 'rechercheartiste':
         $data = Artiste::rechercheArtistes($_GET['stringrecherche']);
         break;
+
+    case 'historique':
+        switch ($request_method) {
+            case 'GET':
+                $data = User::historiqueUser($_SESSION['user']['id_user']);
+                break;
+            case 'POST':
+
+                if (isset($_SESSION["user"])) {
+                    if (User::isInHistorique($_SESSION["user"]['id_user'], $_POST['id'])) {
+                        User::changeInHistorique($_SESSION['user']['id_user'], $_POST['id'], date("Y-m-d H:i:s"));
+                    } else {
+                        User::ajoutHistorique($_SESSION["user"]['id_user'], $_POST['id']);
+                    }
+                    $data = true;
+                } else {
+                    $data = false;
+                }
+
+                break;
+            default:
+                // Requête invalide
+                header("HTTP/1.0 405 Method Not Allowed");
+                break;
+
+        }
+        break;
+
     default:
         // Requête invalide
         header("HTTP/1.0 405 Method Not Allowed");
         break;
 }
-
 
 echo json_encode($data);
 exit;
